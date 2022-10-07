724f895ba4d5f283fccffc4849a9f54ba5e495b1
ghongli <18810027593@163.com>
2022-10-07 20:00:26 +0800

    perform the search for the specified term

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+	
+	_ "github.com/dapings/sample/matchers"
+	"github.com/dapings/sample/search"
+)
+
+var logPrefix = "dapings sample "
+
+func init() {
+	log.SetPrefix(logPrefix)
+	// change the device for logging to stdout.
+	log.SetOutput(os.Stdout)
+}
+
 func main() {
-	$END$
+	// perform the search for the specified term.
+	search.Run("president")
 }
